18d52bcce8f76839154a29f6721e542ea018c1fc
LoganEvans <loganpevans@gmail.com>
2013-10-19 13:38:58 -0700

    Added some code to create mock entries in the db.

--- tools/amp_db_create.py
+++ tools/amp_db_create.py
@@ -1,19 +1,30 @@
 import sqlite3
+import datetime
+
 conn = sqlite3.connect('amp.db')
 
 def create_db_tables():
     curs = conn.cursor()
+    curs.execute("""
+            CREATE TABLE "android_metadata" (
+                "locale" TEXT DEFAULT 'en_US'
+            )
+    """)
+
+    curs.execute("""
+            INSERT INTO "android_metadata" VALUES ('en_US')
+    """)
 
     curs.execute("""
             CREATE TABLE user(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 name        TEXT
             )
     """)
 
     curs.execute("""
             CREATE TABLE stellar_object(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 ngc_ic      TEXT,
                 name        TEXT,
                 description TEXT,
@@ -29,7 +40,7 @@ def create_db_tables():
 
     curs.execute("""
             CREATE TABLE docket(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 name        TEXT,
                 observations    INTEGER
             )
@@ -37,17 +48,17 @@ def create_db_tables():
 
     curs.execute("""
             CREATE TABLE docket_item(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 docket_id   INTEGER,
                 stellar_object_id   INTEGER,
-                FOREIGN KEY(docket_id) REFERENCES docket(id),
-                FOREIGN KEY(stellar_object_id) REFERENCES stellar_object(id)
+                FOREIGN KEY(docket_id) REFERENCES docket(_id),
+                FOREIGN KEY(stellar_object_id) REFERENCES stellar_object(_id)
             )
     """)
 
     curs.execute("""
             CREATE TABLE telescope(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 name        TEXT,
                 type        TEXT,
                 manufacturer    TEXT,
@@ -57,18 +68,18 @@ def create_db_tables():
 
     curs.execute("""
             CREATE TABLE session(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id         INTEGER PRIMARY KEY AUTOINCREMENT,
                 started     DATETIME,
                 telescope_id    INTEGER,
                 user_id     INTEGER,
-                FOREIGN KEY(telescope_id) REFERENCES telescope(id),
-                FOREIGN KEY(user_id) REFERENCES user(id)
+                FOREIGN KEY(telescope_id) REFERENCES telescope(_id),
+                FOREIGN KEY(user_id) REFERENCES user(_id)
             )
     """)
 
     curs.execute("""
             CREATE TABLE session_item(
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                _id          INTEGER PRIMARY KEY AUTOINCREMENT,
                 session_id  INTEGER,
                 stellar_object_id   INTEGER,
                 time        DATETIME,
@@ -77,10 +88,40 @@ def create_db_tables():
                 zipcode     INTEGER,
                 gps_latitude    REAL,
                 gps_longitude   REAL,
-                FOREIGN KEY(session_id) REFERENCES session(id),
-                FOREIGN KEY(stellar_object_id) REFERENCES stellar_object(id)
+                FOREIGN KEY(session_id) REFERENCES session(_id),
+                FOREIGN KEY(stellar_object_id) REFERENCES stellar_object(_id)
             )
     """)
 
     conn.commit()
+
+
+def populate_mock_entries():
+    """Assumes that the database is empty."""
+    curs = conn.cursor()
+    queries = [
+        ("INSERT INTO user VALUES(?, ?)", (None, "John Smith",)),
+        ("""
+            INSERT INTO stellar_object
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+         """,
+         (None, "200000", "foobar", "foobar", "mock",
+          9000.1, "10x20", None, None, None, None)),
+        ("INSERT INTO docket VALUES(?, ?, ?)", (None, "mock", 1)),
+        ("INSERT INTO docket_item VALUES (?, ?, ?)", (None, 1, 1)),
+        ("INSERT INTO telescope VALUES (?, ?, ?, ?, ?)",
+         (None, "My scope", "refractor", "Orion", 6.0)),
+        ("INSERT INTO session VALUES (?, ?, ?, ?)",
+         (None, datetime.datetime.now(), 1, 1)),
+        ("INSERT INTO session_item VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+         (None, 1, 1, datetime.datetime.now(), "bad", "big", 83843, 123.4, 567.8))
+    ]
+    for query, values in queries:
+        curs.execute(query, values)
+
+    conn.commit()
+
+if __name__ == '__main__':
+    create_db_tables()
+    populate_mock_entries()
 
