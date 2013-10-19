661630696807e55a040ec5f68009602e28574829
cevans87 <c.d.evans87@gmail.com>
2013-10-19 12:15:07 -0700

    Replaced default Dummy files with slightly less dumb files.

---
+++ AstronoMYpal/src/main/java/com/bigtheta/astronomypal/dummy/MainMenuPictureFragment.java
@@ -0,0 +1,9 @@
+package com.bigtheta.astronomypal.dummy;
+
+/**
+ * Created by cevans on 10/19/13.
+ */
+public class MainMenuPictureFragment {
+    /*TODO replace ItemDetailFragment with this. Make this display a pretty picture
+      for the main menu. */
+}
