ca6d3907a8de83dff71355ee5841cf6f796b4b6f
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-10-19 13:40:32 -0700

    Adding caldwell parser

---
+++ tools/caldwell_parser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from string import strip
+
+data_file = '/Users/andrew/Documents/projects/hackathon/AstronoMYpal/caldwell'
+with open(data_file) as file_:
+    data_list = file_.readlines()
+
+split_data_list = [map(strip, i.split('\t')) for i in data_list]
+
+data_attributes = ['id', 'ngc_id', 'constellation', 'type',
+                   'right_ascension', 'declination',
+                   'magnitude', 'size', 'description']
+
+data_dict_list = [dict(zip(data_attributes, i)) for i in split_data_list]
