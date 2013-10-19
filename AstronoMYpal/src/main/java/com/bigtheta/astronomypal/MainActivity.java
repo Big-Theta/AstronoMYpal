07d59fb66512914a5e3206cd5e9bdfc3c1624411
cevans87 <c.d.evans87@gmail.com>
2013-10-19 15:30:56 -0700

    Added a hack button for Logan.

--- AstronoMYpal/src/main/java/com/bigtheta/astronomypal/MainActivity.java
+++ AstronoMYpal/src/main/java/com/bigtheta/astronomypal/MainActivity.java
@@ -3,6 +3,8 @@ package com.bigtheta.astronomypal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 /**
@@ -77,5 +79,8 @@ public class MainActivity extends FragmentActivity
             detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+    }
+    public void LoganHackTime(View view) {
+        ((TextView) findViewById(R.id.LoganHackTextView)).setText("And God killed another puppy.");
     }
 }
