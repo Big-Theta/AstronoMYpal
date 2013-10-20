54bd440bcd76dcc69189ea8a0243dc75e9d79604
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-10-19 18:45:21 -0700

    Getting correct url and downloading

--- tools/caldwell_img_getter.py
+++ tools/caldwell_img_getter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
+from urllib import urlretrieve
 
 wiki_file = './caldwell_wiki_data.html'
 with open(wiki_file) as file_:
@@ -9,11 +10,14 @@ soup = BeautifulSoup(data, "lxml")
 rows = soup.findAll('tr')
 image_count = 1
 images = {}
-print(rows[1].findAll('td')[3].find('a').get('href'))
+# print(rows[1].findAll('td')[3].find('a').find('img').get('src'))
 for row in rows:
     cells = row.findAll('td')
-    image_name = cells[3].find('a')
+    image_name = cells[3].find('a').find('img')
     if image_name:
         dest_name = "caldwell_{}.jpg".format(image_count)
-        images[dest_name] = "https://en.wikipedia.org"+image_name.get('href')
+        images[dest_name] = "http:"+image_name.get('src')
         image_count += 1
+
+for dest, src in images.items():
+    urlretrieve(src, './assets/'+dest)
